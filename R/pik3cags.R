9509aef68483f545ddbaa5b9e0227dd4626fc133
Markus Schroeder <mschroed@jimmy.harvard.edu>
2011-05-19 17:45:04 +0000

    updating package 2. major bugfixes in subtypes prediction functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genefu@55715 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pik3cags.R
+++
@@ -1,8 +0,0 @@
-`pik3cags` <-
-function(data, annot, do.mapping=FALSE, mapping, verbose=FALSE) {
-
-	pik3cags.gl <- sig.pik3cags[ ,c("probe", "EntrezGene.ID", "coefficient")]
-	res <- sig.score(x=pik3cags.gl, data=data, annot=annot, do.mapping=do.mapping, mapping=mapping, signed=TRUE, verbose=verbose)$score
-	
-	return (res)
-}
