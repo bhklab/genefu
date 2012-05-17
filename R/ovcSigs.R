68c2a59a38d5ffb141eefedda5ff357545f7d718
Benjamin Haibe-Kains <bhaibeka@ircm.qc.ca>
2012-05-17 13:04:05 -0400

    add files for ovarian signatures

---
+++ R/ovcSigs.R
@@ -0,0 +1,6 @@
+`.ovcSigs` <-
+function(sigs=c("bentink2012_angiogenic", "crijns2009_sig", "yoshihara2010_sig", "spentzos2011_sig", "tcga2011_sig")) {
+    for(i in 1:length(sigs)) {
+        sig ≤- read.csv(system.file(file.path("extdata", sprintf("%s.csv", sigs[i])), package="genefu"), stringsAsFactors=TRUE)
+    }
+}
