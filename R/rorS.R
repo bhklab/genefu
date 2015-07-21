0a55668de551b5025d8a045ffca854ec4f9687e8
Markus Schroeder <mschroed@jimmy.harvard.edu>
2015-07-21 10:28:25 +0000

    Major genefu update to version 2
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genefu@106602 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rorS.R
+++ R/rorS.R
@@ -8,7 +8,7 @@ function(data, annot, do.mapping=FALSE, mapping, verbose=FALSE) {
   ## ROR-S
   rs.unscaled <- rs <- rsrisk <- rep(NA, nrow(data))
   names(rs.unscaled) <- names(rs) <- names(rsrisk) <- rownames(data)
-  rst <- 0.05 * sbts$cor[ , "Basal"] + 0.12 * sbts$cor[ , "Her2"] - 0.34 * sbts$cor[ , "LumA"] + 0.23 * sbts$cor[ , "LumA"]
+  rst <- 0.05 * sbts$cor[ , "Basal"] + 0.12 * sbts$cor[ , "Her2"] - 0.34 * sbts$cor[ , "LumA"] + 0.23 * sbts$cor[ , "LumB"]
   rs.unscaled[names(rst)] <- rst
   ## rescale between 0 and 100
   rs <- (rs.unscaled - quantile(rs.unscaled, probs=0.025, na.rm=TRUE)) / (quantile(rs.unscaled, probs=0.975, na.rm=TRUE) - quantile(rs.unscaled, probs=0.025, na.rm=TRUE)) * 100
